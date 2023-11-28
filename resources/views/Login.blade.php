581f1bd1dce21d99653cedf2e8c4efb9aa78c658
laksmanadpm <laksmanadpmaulana@gmail.com>
2023-11-28 08:32:22 +0700

    new

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+<h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Login Yuk Bang</h2>
+    
+</body>
+</html>
